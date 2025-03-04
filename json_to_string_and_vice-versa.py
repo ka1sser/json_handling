eac9cbd16658049c6a1f63ac54ebc9a9cae8d701
ka1sser <serenokaisser.dev@gmail.com>
2025-03-04 15:10:56 +0800

    Added some comments

--- json_to_string_and_vice-versa.py
+++ json_to_string_and_vice-versa.py
@@ -19,7 +19,7 @@ people_string = """
 
 # Convert string to json
 print(f"Type of people_string: {type(people_string)}")
-data = json.loads(people_string)
+data = json.loads(people_string)  # Loading a string (load-S for string)
 print(f"Type of data: {type(data)}")
 
 # Accessing an element and deleting an element
@@ -28,5 +28,7 @@ for person in data["people"]:
 
 # Convert json to string
 print(f"Type of data: {type(data)}")
-new_data = json.dumps(data, indent=2, sort_keys=True)
+new_data = json.dumps(
+    data, indent=2, sort_keys=True
+)  # Dumping a string (dump-S for string)
 print(f"Type of new_data: {type(new_data)}")
