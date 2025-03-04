e7b5ac3a1d52507a359936386f2ee06920bf2383
ka1sser <serenokaisser.dev@gmail.com>
2025-03-04 15:07:35 +0800

    jason to string and vice-versa

---
+++ json_to_string_and_vice-versa.py
@@ -0,0 +1,32 @@
+import json
+
+people_string = """
+{
+    "people": [
+        {
+            "name": "Itadori Yuji",
+            "anime": "Jujutsu Kaisen",
+            "has_won": true
+        },
+        {
+            "name": "Edward Elric",
+            "anime": "Fullmetal Alchemist Brotherhood",
+            "has_won": true
+        }
+    ]
+}
+"""
+
+# Convert string to json
+print(f"Type of people_string: {type(people_string)}")
+data = json.loads(people_string)
+print(f"Type of data: {type(data)}")
+
+# Accessing an element and deleting an element
+for person in data["people"]:
+    del person["has_won"]
+
+# Convert json to string
+print(f"Type of data: {type(data)}")
+new_data = json.dumps(data, indent=2, sort_keys=True)
+print(f"Type of new_data: {type(new_data)}")
